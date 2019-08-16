16a6cd8a80247f9e63ac491c1f46f707aa32ac96
daniela rodriguez <daniela_rodriguez1990@outlook.com>
2019-08-16 18:44:46 -0400

    Added Login Modal/Logout/Register

--- client/src/store.js
+++ client/src/store.js
@@ -4,10 +4,13 @@ import rootReducer from './reducers';
 
 const initialState = {};
 
-const middleware = [thunk];
+const middleWare = [thunk];
 
-const store = createStore(rootReducer, initialState, compose(
-    applyMiddleware(...middleware),
-));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...middleWare))
+);
 
 export default store;
