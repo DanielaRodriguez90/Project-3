4db63161264152bf0d5a8c4dd9a2d2c68f8ca8d9
daniela rodriguez <daniela_rodriguez1990@outlook.com>
2019-08-24 11:15:23 -0400

    deploy to heroku fixes

--- server.js
+++ server.js
@@ -32,8 +32,8 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+        res.sendFile(path.resolve(__dirname, "./client/build/index.html"))
+    });
 }
 
 const PORT = process.env.PORT || 5000;
